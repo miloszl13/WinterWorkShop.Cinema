5541a6c21344e78a9be3326ae587898771b15e99
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-01 05:16:50 -0800

    Updated movies model

--- WinterWorkShop.Cinema.Data/Models/MovieModel.cs
+++ WinterWorkShop.Cinema.Data/Models/MovieModel.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+        public double Rating { get; set; }
     }
 }
