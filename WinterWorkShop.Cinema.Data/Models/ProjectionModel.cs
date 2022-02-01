72155a95595a9dc5c405ceac3a47f41e212d681e
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-01 05:39:20 -0800

    Projection model created

---
+++ WinterWorkShop.Cinema.Data/Models/ProjectionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WinterWorkShop.Cinema.Data.Models
+{
+    public class ProjectionModel
+    {
+        public int ProjectionId { get; set; }
+        public DateTime Date { get; set; }
+        public string AuditoriumName { get; set; }
+    }
+}
