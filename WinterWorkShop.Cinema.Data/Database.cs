3d68b27cf52fb9d72c77f44335aba2f12f4eb742
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-02 04:42:13 -0800

    ProjectionRepository and ProjectionController changes

--- WinterWorkShop.Cinema.Data/Database.cs
+++ WinterWorkShop.Cinema.Data/Database.cs
@@ -13,10 +13,10 @@ namespace WinterWorkShop.Cinema.Data
         
         public List<ProjectionModel> Projections = new List<ProjectionModel>()
         {
-            new ProjectionModel{ ProjectionId=1,Date=DateTime.Now,AuditoriumName="MediumAuditorium",MovieID=2},
-            new ProjectionModel{ ProjectionId=2,Date= Convert.ToDateTime("02/03/2022"),AuditoriumName="SmallAuditorium",MovieID=1},
-            new ProjectionModel{ ProjectionId=3,Date= Convert.ToDateTime("02/05/2022"),AuditoriumName="SmallAuditorium",MovieID=1},
-            new ProjectionModel{ ProjectionId=4,Date= Convert.ToDateTime("02/06/2022"),AuditoriumName="MediumAuditorium",MovieID=2},
+            new ProjectionModel{ ProjectionId=1,Date=Convert.ToDateTime("02/03/2022 19:00:00"),AuditoriumName="MediumAuditorium",MovieID=2},
+            new ProjectionModel{ ProjectionId=2,Date= Convert.ToDateTime("02/03/2022 15:00:00"),AuditoriumName="SmallAuditorium",MovieID=1},
+            new ProjectionModel{ ProjectionId=3,Date= Convert.ToDateTime("02/05/2022 18:00:00"),AuditoriumName="SmallAuditorium",MovieID=1},
+            new ProjectionModel{ ProjectionId=4,Date= Convert.ToDateTime("02/06/2022 20:00:00"),AuditoriumName="MediumAuditorium",MovieID=2},
         };
     }
 }
